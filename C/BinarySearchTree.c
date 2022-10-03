727097cf09bb2cccd15f64f80bb31c2c4a16af14
Jake Johnson <jakejohnson818@gmail.com>
2022-10-03 15:16:53 -0600

    Everything running, hopefully correctly

--- C/BinarySearchTree.c
+++ C/BinarySearchTree.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int factorial(int);
+int numOfTree(int);
 
 int main() {
+    printf("Total number of Binary Search Trees with given key: %d", numOfTree(5));
     return 0;
+}
+
+int factorial(int num){
+    int fact = 1;
+    if(num == 0){
+        return 1;
+    } else {
+        while (num > 1){
+            fact = fact * num;
+            num--;
+        }
+        return fact;
+    }
+}
+
+int numOfTree(int key){
+    int catalanNumber = factorial(2 * key)/(factorial(key + 1) * factorial(key));
+    return catalanNumber;
 }
--- C/SinglyLinkedList.c
+++ C/SinglyLinkedList.c
@@ -1,5 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+struct Node{
+    int data;
+    struct Node* next;
+
+};
+
+struct Node *head = NULL;
+struct Node *current = NULL;
+
+void addNode(struct Node** head, int data);
+void showNodes(struct Node* node);
 
 int main() {
+    addNode(&head, 1);
+    addNode(&head, 2);
+    addNode(&head, 3);
+    addNode(&head, 4);
+
+    showNodes(head);
+
     return 0;
 }
+
+void addNode(struct Node** head, int data){
+    struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+
+    newNode->data = data;
+    newNode->next = *head;
+
+    *head = newNode;
+};
+
+void showNodes(struct Node* node){
+    printf("/nYour linked list is:");
+
+    while (node != NULL){
+        printf("%d", node->data);
+        node = node->next;
+    }
+    
+    printf("\n");
+};
